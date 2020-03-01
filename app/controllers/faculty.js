c53d0d1446ad7c630e6465f16f34b21de4047553
Teja Sree <tejasree8319@gmail.com>
2020-03-01 13:47:37 +0530

    Added faculty controller code and faculty API's in postman file

--- app/controllers/faculty.js
+++ app/controllers/faculty.js
@@ -6,9 +6,91 @@ const _=require('lodash');
 console.log("Execute");
 
 const getFaculty=(req,res)=>{
-    res.send("Welcome");
+    const facultyId =req.params.facultyId
+    Faculty.findOne({facultyId})
+    .then(faculty => {
+        if(!faculty) {
+            return res.status(404).send({
+                message: "Faculty not found with id " + req.params.facultyId
+            });            
+        }
+        res.send(faculty)
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Faculty not found with id " + req.params.facultyId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving faculty with id " + req.params.facultyId
+        });
+    });
 };
 
+const getAllFaculty=(req,res)=>{
+    Faculty.find()
+.then( facultyData=> {
+    res.send(facultyData);
+}).catch(err => {
+    res.status(500).send({
+        message: err.message || "Some error occurred while retrieving faculty data."
+    });
+});
+};
+
+const pick=body=>_.pick(body,['facultyName','facultyGender','facultyExperience','facultyDept','facultyEmail','facultyContact','facultyAddress']);
+
+// Find note and update it with the request body
+const updateFaculty=(req,res)=>{
+    console.log("contl");
+  console.log(Object.keys(req.body))
+  
+  console.log(pick);
+  const facultyId = req.params.facultyId;
+Faculty.findOneAndUpdate({facultyId},
+    pick(req.body),
+    {new: true}
+    )
+.then(id => {
+  if(!id) {
+      return res.status(404).send({
+          message: "Faculty not found with id " + req.params.facultyId
+      });
+  }
+  res.send(id);
+}).catch(err => {
+  if(err.kind === 'ObjectId') {
+      return res.status(404).send({
+          message: "Faculty not found with id " + req.params.facultyId
+      });                
+  }
+  return res.status(500).send({
+      message: "Error updating faculty with id " + req.params.facultyId
+  });
+});
+};
+
+const deleteFaculty = (req, res) => {
+    const facultyId = req.params.facultyId;
+    Faculty.findOneAndRemove({facultyId})
+    .then(id => {
+        if(!id) {
+            return res.status(404).send({
+                message: "Faculty not found with id " + req.params.facultyId
+            });
+        }
+        res.send({message: "Faculty deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Faculty not found with id " + req.params.facultyId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete faculty with id " + req.params.facultyId
+        });
+    });
+};
 
 // Create a Note
 
@@ -42,8 +124,11 @@ const createFaculty = async (req,res)=>{
 
 
 
-facultyRouter.get('/',getFaculty)
+facultyRouter.get('/all',getAllFaculty)
+facultyRouter.get('/:facultyId',getFaculty)
 facultyRouter.post('/',createFaculty)
+facultyRouter.patch('/:facultyId',updateFaculty)
+facultyRouter.delete('/:facultyId',deleteFaculty)
 
 module.exports = facultyRouter;
 
